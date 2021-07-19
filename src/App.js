af51e9c9f55fb7df499b4af0159de821d6f389ab
beror <beror>
2021-07-19 10:35:28 +0300

    Changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import MainTable from './MainTable';
 import FloatingButton from './FloatingButton';
